7258e78fb029bfc5c35f004b2f774c8473c4eaa8
xiaocai-xing <cxqq2580@foxmail,com>
2021-11-16 17:07:06 +0800

    添加必备jar包

--- src/main/java/com/example/springproject/SpringprojectApplication.java
+++ src/main/java/com/example/springproject/SpringprojectApplication.java
@@ -2,8 +2,9 @@ package com.example.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringprojectApplication {
 
     public static void main(String[] args) {
